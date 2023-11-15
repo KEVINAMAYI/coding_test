145ef170574db6d2d590c164e0d941e480c4b3eb
KEVINAMAYI <kevinamayi20@gmail.com>
2023-11-15 14:44:09 +0300

    Updated Readme

--- vue-rest-api/src/views/EditProductView.vue
+++ vue-rest-api/src/views/EditProductView.vue
@@ -25,7 +25,7 @@ onMounted( async ()=>{
           class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Add Product
+            Edit Product
           </h1>
           <form @submit.prevent="productStore.updateProduct(props.id)" class="space-y-4 md:space-y-6">
             <div>
